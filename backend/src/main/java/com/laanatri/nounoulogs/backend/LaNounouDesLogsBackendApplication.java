1a8dbd098654b3543a0ee75cb012c1a197b60b52
laanatri <laanatri.ade@gmail.com>
2025-12-05 18:31:21 +0100

    build(init spring): initialization backend spring

---
+++ backend/src/main/java/com/laanatri/nounoulogs/backend/LaNounouDesLogsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laanatri.nounoulogs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaNounouDesLogsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaNounouDesLogsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/laanatri/nounoulogs/backend/LaNounouDesLogsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laanatri.nounoulogs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaNounouDesLogsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
