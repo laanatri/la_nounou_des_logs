2c3892945fb11098351b744fb22f223a1373a3aa
laanatri <laanatri.ade@gmail.com>
2026-01-21 14:40:19 +0100

    fix(health): update import Map

--- backend/src/main/java/com/nounou/nounou/controller/HealthController.java
+++ backend/src/main/java/com/nounou/nounou/controller/HealthController.java
@@ -1,11 +1,11 @@
 package com.nounou.nounou.controller;
 
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
